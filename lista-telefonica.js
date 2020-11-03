724dba84e2d07f14774e85b690ab0861fd626b59
Welbber <welbervital@gmail.com>
2020-11-03 15:36:39 -0300

    Criação do servidor json, integrando com ajax e trablhando com filtros

--- lista-telefonica.js
+++ lista-telefonica.js
@@ -6,62 +6,92 @@
     angular.module('listaTelefonica', ["ngMessages"]);
 
     angular.module('listaTelefonica').controller("listaTelefonicaCtrl", listaTelefonicaCtrl);
-    
-    listaTelefonicaCtrl.$inject = ['$scope'];
 
-    function listaTelefonicaCtrl($scope){
+    listaTelefonicaCtrl.$inject = ['$scope', '$http'];
+
+    function listaTelefonicaCtrl($scope, $http) {
         var vm = this;
 
         //variaveis
         vm.app = "Lista Telefonica";
         vm.classe = "selecionado";
-     
-       
-        vm.contatos = [
-            {nome : 'Welbber', telefone: "1234-1234", cor: "red", data: new Date()},
-            {nome : 'jose', telefone: "4321-4321", cor: "yellow", data: new Date()},
-            {nome : 'joao', telefone: "4321-1234", cor : "blue", data: new Date()},
-            {nome : 'pedro', telefone: "4322-2234", cor: "orange", data: new Date()},
-        ];
-        vm.operadoras = [
-            {nome: "Oi", codigo: 88, categoria:"Celular", preco: 1},
-            {nome: "Claro", codigo: 93, categoria:"Celular", preco: 2},
-            {nome: "Tim", codigo: 91, categoria:"Celular",  preco: 3},
-            {nome: "Vivo", codigo: 84, categoria:"Celular", preco: 5},
-            {nome: "GVT", codigo: 25, categoria:"Fixo", preco: 0.5},
-            {nome: "Embratel", codigo: 21, categoria:"Fixo", preco: 1.5}
-        ];
-        
+
+
+        vm.contatos = [];
+        vm.operadoras = [];
+
         // Variaveis de funcoes
         vm.adicionarContato = adicionarContato;
         vm.apagarContato = apagarContato;
         vm.isContatoSelecionado = isContatoSelecionado;
         vm.ordenarPor = ordenarPor
-        
+
         //funcoes
+        function recuperaContatos() {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/contato'
+            }).then(function successCallback(response) {
+                vm.contatos = response.data;
+            }, function errorCallback(response) {
+                alert("Erro ao solicitar dados do servidor");
+            });
+        }
 
-        function adicionarContato(contato){
-            vm.contatos.push(angular.copy(contato));
-            delete $scope.contato;
-            $scope.contatoForm.$setPristine();
-        };
-
-        function apagarContato(contatos){
-             vm.contatos = contatos.filter(function (contato){
-                 if (!contato.selecionado ) return contato;
-             })
-             
-        };
-        
-        function isContatoSelecionado(contatos){
+        function recuperaOperadoras() {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:3000/operadora'
+            }).then(function successCallback(response) {
+                vm.operadoras = response.data;
+            }, function errorCallback(response) {
+                alert("Erro ao solicitar dados do servidor");
+            });
+        }
+        recuperaContatos();
+        recuperaOperadoras();
+        function adicionarContato(contato) {
+           
+            contato.data = new Date();
+            contato.id = vm.contatos.length + 1;
             
-            return contatos.some(function (contato){
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/contato/',
+                data: contato
+            }).then(function successCallback(response) {
+                recuperaContatos();
+            }, function errorCallback(response) {
+                alert("Erro ao salvar contato no servidor");
+            });
+        }
+     
+        function apagarContato(contatos) {
+            console.log(contatos);
+            contatos.filter(function (contato) {
+                if (!contato.selecionado) {
+                    $http({
+                        method: 'DELETE',
+                        url: 'http://localhost:3000/contato/' + contato.id
+                    }).then(function successCallback(response) {
+                        recuperaContatos();
+                    }, function errorCallback(response) {
+                        alert("Erro ao solicitar dados do servidor");
+                    });
+                };
+            })
+
+        };
+
+        function isContatoSelecionado(contatos) {
+
+            return contatos.some(function (contato) {
                 return contato.selecionado;
             });
-            
+
         };
-        
-        function ordenarPor(campo){
+
+        function ordenarPor(campo) {
             vm.criterioDeOrdenacao = campo;
             vm.direcaoDaOrdenacao = !vm.direcaoDaOrdenacao;
         }
