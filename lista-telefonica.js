0bc1f66ff308df460bec264dee492b55ca5d290d
Welbber <welbervital@gmail.com>
2020-11-03 11:05:18 -0300

    Criacao footer e trabalhando com diretivas

--- lista-telefonica.js
+++ lista-telefonica.js
@@ -13,19 +13,43 @@
         var vm = this;
         vm.app = "Lista Telefonica";
         vm.contatos = [
-            {nome : 'Welbber', telefone: "9999999"},
-            {nome : 'jose', telefone: "9999999"},
-            {nome : 'joao', telefone: "9999999"},
-            {nome : 'pedro', telefone: "9999999"},
+            {nome : 'Welbber', telefone: "9999999", cor: "red"},
+            {nome : 'jose', telefone: "9999999", cor: "yellow"},
+            {nome : 'joao', telefone: "9999999", cor : "blue"},
+            {nome : 'pedro', telefone: "9999999", cor: "orange"},
         ];
         vm.adicionarContato = adicionarContato;
-
+        vm.apagarContato = apagarContato;
+        vm.isContatoSelecionado = isContatoSelecionado;
+        vm.classe = "selecionado";
+        vm.operadoras = [
+            {nome: "Oi", codigo: 88, categoria:"Celular"},
+            {nome: "Claro", codigo: 93, categoria:"Celular"},
+            {nome: "Tim", codigo: 91, categoria:"Celular"},
+            {nome: "Vivo", codigo: 84, categoria:"Celular"},
+            {nome: "GVT", codigo: 25, categoria:"Fixo"},
+            {nome: "Embratel", codigo: 21, categoria:"Fixo"}
+        ];
+        
         function adicionarContato(contato){
             vm.contatos.push(angular.copy(contato));
             // ou
             // delete $scope.contato;
         };
-    }
 
-    
+        function apagarContato(contatos){
+             vm.contatos = contatos.filter(function (contato){
+                 if (!contato.selecionado ) return contato;
+             })
+             
+        };
+        
+        function isContatoSelecionado(contatos){
+            
+            return contatos.some(function (contato){
+                return contato.selecionado;
+            });
+            
+        };
+    }
 })();
