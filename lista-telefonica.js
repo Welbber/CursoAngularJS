c999edf21c9c4981595925328d452c576a20f73d
Welbber <welbervital@gmail.com>
2020-11-03 11:09:21 -0300

    exclusao do arquivo footer e trabalhando com validacao de formuario

--- lista-telefonica.js
+++ lista-telefonica.js
@@ -3,7 +3,7 @@
 
     // MODULO
 
-    angular.module('listaTelefonica', []);
+    angular.module('listaTelefonica', ["ngMessages"]);
 
     angular.module('listaTelefonica').controller("listaTelefonicaCtrl", listaTelefonicaCtrl);
     
@@ -11,30 +11,39 @@
 
     function listaTelefonicaCtrl($scope){
         var vm = this;
+
+        //variaveis
         vm.app = "Lista Telefonica";
+        vm.classe = "selecionado";
+     
+        
         vm.contatos = [
-            {nome : 'Welbber', telefone: "9999999", cor: "red"},
-            {nome : 'jose', telefone: "9999999", cor: "yellow"},
-            {nome : 'joao', telefone: "9999999", cor : "blue"},
-            {nome : 'pedro', telefone: "9999999", cor: "orange"},
+            {nome : 'Welbber', telefone: "1234-1234", cor: "red", data: new Date()},
+            {nome : 'jose', telefone: "4321-4321", cor: "yellow", data: new Date()},
+            {nome : 'joao', telefone: "4321-1234", cor : "blue", data: new Date()},
+            {nome : 'pedro', telefone: "4322-2234", cor: "orange", data: new Date()},
         ];
+        vm.operadoras = [
+            {nome: "Oi", codigo: 88, categoria:"Celular", preco: 1},
+            {nome: "Claro", codigo: 93, categoria:"Celular", preco: 2},
+            {nome: "Tim", codigo: 91, categoria:"Celular",  preco: 3},
+            {nome: "Vivo", codigo: 84, categoria:"Celular", preco: 5},
+            {nome: "GVT", codigo: 25, categoria:"Fixo", preco: 0.5},
+            {nome: "Embratel", codigo: 21, categoria:"Fixo", preco: 1.5}
+        ];
+        
+        // Variaveis de funcoes
         vm.adicionarContato = adicionarContato;
         vm.apagarContato = apagarContato;
         vm.isContatoSelecionado = isContatoSelecionado;
-        vm.classe = "selecionado";
-        vm.operadoras = [
-            {nome: "Oi", codigo: 88, categoria:"Celular"},
-            {nome: "Claro", codigo: 93, categoria:"Celular"},
-            {nome: "Tim", codigo: 91, categoria:"Celular"},
-            {nome: "Vivo", codigo: 84, categoria:"Celular"},
-            {nome: "GVT", codigo: 25, categoria:"Fixo"},
-            {nome: "Embratel", codigo: 21, categoria:"Fixo"}
-        ];
+        vm.ordenarPor = ordenarPor
         
+        //funcoes
+
         function adicionarContato(contato){
             vm.contatos.push(angular.copy(contato));
-            // ou
-            // delete $scope.contato;
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
         };
 
         function apagarContato(contatos){
@@ -51,5 +60,10 @@
             });
             
         };
+        
+        function ordenarPor(campo){
+            vm.criterioDeOrdenacao = campo;
+            vm.direcaoDaOrdenacao = !vm.direcaoDaOrdenacao;
+        }
     }
 })();
